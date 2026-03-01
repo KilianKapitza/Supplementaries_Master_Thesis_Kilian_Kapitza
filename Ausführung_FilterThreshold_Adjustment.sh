5836b63dbe6e7fb9d78968cadeb80e9c738d7002
KilianKapitza <kiliankapitza@gmail.com>
2026-03-01 22:23:08 +0100

    Update Ausführung_FilterThreshold_Adjustment.sh

--- Ausführung_FilterThreshold_Adjustment.sh
+++ Ausführung_FilterThreshold_Adjustment.sh
@@ -1,17 +1,17 @@
   #!/bin/bash
 # Author: Kilian Kapitza  
-# Pfad des Inputs: bereits durch gene_detection_fastq.sh gefilterte fastq
+# Pfad des Inputs: bereits durch gene_detection_fastq.sh gefilterte fastq (match scores of k-mer counts per read are marked in fastq header by gene_detection_fastq.sh)
 # Pfad und Benennung des Outputs: Gefilterte Fastq Datei mit erhöht angepasstem Threshold
 # Einstellen des Thresholds, also der Anzahl an K-mers die pro Read zu finden sein müssen
 # Pfad und Benennung des entstehenden Histogramms
 # Aufpassen, dass richtiges conda env aktiv ist! hier ist es: FilterThreshold_Adjustment_env
 
 # Allgemeine Pfade
-R_SCRIPT="/home/diablo/Desktop/Kilian_Daten/Testphase/FilterThreshold_Adjustment.r"
-OUT_DIR="/home/diablo/Desktop/Kilian_Daten/Testphase/Ergebnisse/NCF1_20251218_AS_Mat5844_Pat_TP11/19-mer/fastq" # Input+Output Path
+R_SCRIPT="/FilterThreshold_Adjustment.r"
+OUT_DIR="/Path/to/your/fastq" # Input+Output Path
 
 # Startdatei: TH0
-INPUT_FASTQ="${OUT_DIR}/combined_results_NCF1_TH0_KM19_Mat5844_Pat_TP11.fastq"	# Welche Startdatei verwendet werden soll
+INPUT_FASTQ="${OUT_DIR}/NCF1_Pat_15mer_TH0.fastq"	# Welche Startdatei verwendet werden soll
 # Liste der Thresholds, die du testen möchtest
 # entweder for THRESHOLD in 50 100 150 200; do für diese 4 Threshold oder for THRESHOLD in $(seq 50 10 200); do für alle Thresholds von 50 bis 200 in 10er Schritten
 # #outdated# für schnelleres sorting: erst einmal TH50 und dann $(seq 60 10 200), um Großteil der Reads aussortiert zu haben, wichtig: auch Input dateiname ändern
